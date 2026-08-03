0b9734727c97f53c2a67dcf407032a1e821fda77
denisMunerolli <63620642+denisMunerolli@users.noreply.github.com>
2026-08-03 15:12:38 -0300

    feat(fase3): migra Curriculo para EntidadeBase

--- smartjobai-core/src/main/java/com/smartjobai/core/entity/Curriculo.java
+++ smartjobai-core/src/main/java/com/smartjobai/core/entity/Curriculo.java
@@ -2,28 +2,22 @@ package com.smartjobai.core.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import lombok.Data;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
-
-import java.time.LocalDateTime;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "curriculos")
-@Data
-public class Curriculo {
+@Getter
+@Setter
+@EqualsAndHashCode(callSuper = true)
+public class Curriculo extends EntidadeBase {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "usuario_id", nullable = false)
     private Usuario usuario;
 
@@ -35,12 +29,4 @@ public class Curriculo {
 
     @Column(name = "conteudo_json", columnDefinition = "TEXT")
     private String conteudoJson;
-
-    @CreationTimestamp
-    @Column(name = "data_criacao", updatable = false)
-    private LocalDateTime dataCriacao;
-
-    @UpdateTimestamp
-    @Column(name = "data_atualizacao")
-    private LocalDateTime dataAtualizacao;
 }
