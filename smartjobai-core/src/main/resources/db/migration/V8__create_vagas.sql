7012af8cb478c3116e5a57ad0221b9438211c761
denisMunerolli <63620642+denisMunerolli@users.noreply.github.com>
2026-08-04 03:14:58 -0300

    fix: adiciona data_criacao e data_atualizacao na criacao da tabela vagas (EntidadeBase)

--- smartjobai-core/src/main/resources/db/migration/V8__create_vagas.sql
+++ smartjobai-core/src/main/resources/db/migration/V8__create_vagas.sql
@@ -1,12 +1,14 @@
 CREATE TABLE vagas (
-    id BIGSERIAL PRIMARY KEY,
-    id_externo VARCHAR(255),
-    fonte VARCHAR(50),
-    titulo VARCHAR(255),
-    empresa VARCHAR(255),
-    descricao TEXT,
-    localizacao VARCHAR(255),
-    data_coleta TIMESTAMP
+    id              BIGSERIAL PRIMARY KEY,
+    id_externo      VARCHAR(255),
+    fonte           VARCHAR(50),
+    titulo          VARCHAR(255),
+    empresa         VARCHAR(255),
+    descricao       TEXT,
+    localizacao     VARCHAR(255),
+    data_coleta     TIMESTAMP,
+    data_criacao    TIMESTAMP NOT NULL DEFAULT NOW(),
+    data_atualizacao TIMESTAMP
 );
 
 CREATE INDEX idx_vaga_fonte_id_externo ON vagas (fonte, id_externo);
