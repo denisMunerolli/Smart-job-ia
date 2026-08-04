e3eb00a9b6f44f4c51577369ffa3f717060c9719
denisMunerolli <63620642+denisMunerolli@users.noreply.github.com>
2026-08-04 09:47:50 -0300

    feat(fase5): adiciona enum StatusCandidatura

---
+++ smartjobai-core/src/main/java/com/smartjobai/core/entity/StatusCandidatura.java
@@ -0,0 +1,11 @@
+package com.smartjobai.core.entity;
+
+public enum StatusCandidatura {
+    PENDENTE,
+    ENVIADA,
+    EM_ANALISE,
+    ENTREVISTA,
+    APROVADA,
+    REPROVADA,
+    DESISTENCIA
+}
